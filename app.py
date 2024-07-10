233d6ba2f650a6d3d49b121e33b3bb65faa60b9b
jassi <jassik2002@yandex.ru>
2024-07-11 00:40:28 +1000

    Create webapp

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, redirect, url_for, make_response
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            resp = make_response(redirect(url_for('welcome')))
+            resp.set_cookie('username', user.username)
+            return resp
+    return render_template('login.html')
+
+
+@app.route('/')
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('index.html', username=username)
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect(url_for('login')))
+    resp.delete_cookie('username')
+    return resp
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
