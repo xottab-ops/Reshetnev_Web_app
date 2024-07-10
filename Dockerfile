f24fb3848fed95b5602862b51535e8c1c06853b8
jassi <jassik2002@yandex.ru>
2024-07-11 07:06:39 +1000

    Create webapp

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.12.3-alpine
 WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-COPY . .
+COPY . /app
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def logout():
     return resp
 
 
-if __name__ == '__main__':
+with app.app_context():
     db.create_all()
+
+
+if __name__ == '__main__':
+
     app.run(debug=True)
